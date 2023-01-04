d1ffb6023cea3c3cee6dd30791e695e1506171da
tttduc <ductrathu@gmail.com>
2023-01-04 23:03:51 +0700

    RJ2728-Ducttt

--- 2-examples/src/App.tsx
+++ 2-examples/src/App.tsx
@@ -1,15 +1,13 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-import anh from './picture.svg';
+import anh from './picture/LOKI.jpg';
 
 function App() {
-  let style = {
-    width: 350
-  }
   return (
-    <div className="card" style={style}>
-  <img src={anh} className="card-img-top"></img>
+    <div className="bigbox">
+    <div className="card" >
+  <img src={anh} className="card-img"></img>
   <div className="card-body">
     <h5 className="card-title text-warning">Trương Thế Trần Đức</h5>
     <p className="card-text text-success">Trường Đại Học Duy Tân</p>
@@ -17,8 +15,9 @@ function App() {
   <ul className="list-group list-group-flush">
     <li className="list-group-item">Chuyên ngành Công nghệ phần mềm</li>
     <li className="list-group-item">K26 TPM7</li>
-    <li className="list-group-item">Yến Nê 1 - Hòa Tiến - Hòa Vang - Đà Nẵng</li>
+    <li className="list-group-item">Hòa Cường Bắc - Hải Châu - Đà Nẵng</li>
   </ul>
+</div>
 </div>
   );
 }
